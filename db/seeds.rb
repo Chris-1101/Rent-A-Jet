d7dd898c8dcafb07d23b9c834ee0517bbe654bb1
amine larhrissi <codemeforlife@gmail.com>
2018-11-13 15:30:19 +0000

    listings page ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@ user = User.all.where(email: "amine@gmail.com")
 
 puts user
 jet = Jet.create!(name: "FK-232", model: "FK", city: "Los Angeles", price_hour: 200, seats: 3, user_id: 1)
-puts " created #{jet.name} and it belongs to #{jet.user}"
+puts " created #{jet.name} and it belongs to #{jet.user.email}"
 jet = Jet.create!(name: "TX-454" , model: "TX" , city: "Dubai", price_hour: 300 , seats: 4, user_id: 1)
-puts " created #{jet.name} and it belongs to #{jet.user}"
+puts " created #{jet.name} and it belongs to #{jet.user.email}"
 jet = Jet.create!(name: "TJ-454" , model: "TJ" , city: "Paris", price_hour: 700 , seats: 7, user_id: 1)
-puts " created #{jet.name} and it belongs to #{jet.user}"
-jet = Jet.create!(name: "KW-454" , model: "KW" , city: "New York", price_hour: 500 , seats: 5, user_id: 1)
-puts " created #{jet.name} and it belongs to #{jet.user}"
-jet = Jet.create!(name: "WX-454" , model: "WX" , city: "Madrid", price_hour: 400 , seats: 5, user_id: 1)
-puts " created #{jet.name} and it belongs to #{jet.user}"
+puts " created #{jet.name} and it belongs to #{jet.user.email}"
+# jet = Jet.create!(name: "KW-454" , model: "KW" , city: "New York", price_hour: 500 , seats: 5, user_id: 1)
+# puts " created #{jet.name} and it belongs to #{jet.user.email}"
+# jet = Jet.create!(name: "WX-454" , model: "WX" , city: "Madrid", price_hour: 400 , seats: 5, user_id: 1)
+# puts " created #{jet.name} and it belongs to #{jet.user.email}"
